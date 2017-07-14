535ba1ea5cd8623fc3a36b22fcf939bdad827a0b
Derrick Gillespie <derrickgillespiewv@gmail.com>
2017-07-14 14:55:47 -0400

    returns same (x-1)

--- close_bnc.rb
+++ close_bnc.rb
@@ -16,6 +16,7 @@
 # same
 # end
 
+# This was the first task.
 def lotto_func(a, b)
 same = [] 
 b.each_with_index do |value, index|
@@ -29,50 +30,48 @@ end
 same
 end
 
-
+#This was checking for 3/4 numbers.
 def one_off(a, b)
 	index_position = 0
 	count = 0
-	
+
 	b.each do |winning_ticket|
 	count = 0
 	index_position = 0
-	4.times do 
+	a.length.times do 
 		if a[index_position] == winning_ticket[index_position]
 		count += 1
 		end
 		index_position += 1
 	end
 end
-	count == 3
+	count == a.length - 1
 end
 
+# This was first 3/4 numbers that are returned. It now returns numbers that are x-1 the same. 
+def three_out_four(a, b)
+	matches = []
+	index_position = 0
+	count = 0
+	
+	b.each do |winning_ticket|
+		count = 0
+		index_position = 0
+		a.length.times do 
+			if a[index_position] == winning_ticket[index_position]
+				count += 1
+			end
+			index_position += 1
+		end
 
-# def off_by_one(a, b)
-#   count = 0
-#   index_a = 0
-#    b.length.times do
-#    index_b = 0
-#     b.length.times do
-#       if a[index_a] == b[index_b]
-#         count += 1
-#       end
-#       index_b+=1
-#   end
-#   index_a+=1
-# end
-#   count == b.length - 1
-# end
+		if count == winning_ticket.length - 1
+		matches << winning_ticket
+		end
+	end
 
-
-# def three_out_four(a, b)
-#   same = []
-#   b.each do |y|
-#     if off_by_one(a, y) == true
-#       same << y
-#     end
-#   end
-#   winning
-# end
+	if count == a.length - 1	
+end
+ matches
+end
 
 # one_off "1234", ["1234","5678", "9012", "5555"]
--- close_bnc_test.rb
+++ close_bnc_test.rb
@@ -32,9 +32,45 @@ class TestClose_BNC < Minitest::Test
 
     def test_winning_one_off
     	my_number = "1357"
-    	winning_numbers = ["1358","3157","1350"]
+    	winning_numbers = ["1358","7777","1350"]
     	assert_equal(true, one_off(my_number, winning_numbers))
 	end
-end
 
+	def test_winning_one_off_long
+    	my_number = "13579"
+    	winning_numbers = ["13589","77777","13509"]
+    	assert_equal(true, one_off(my_number, winning_numbers))
+	end
+
+	def test_three_out_four
+		my_number = "1357"
+    	winning_numbers = ["1358","7777","1350"]
+    	assert_equal(["1358","1350"], three_out_four(my_number, winning_numbers))
+    end
+
+	def test_three_out_four_remix
+		my_number = "1357"
+    	winning_numbers = ["1347","7777","1257"]
+    	assert_equal(["1347","1257"], three_out_four(my_number, winning_numbers))
+    end
+
+	def test_three_out_four_single
+		my_number = "1357"
+    	winning_numbers = ["1358"]
+    	assert_equal(["1358"], three_out_four(my_number, winning_numbers))
+    end
+
+    def test_X_of_Y
+    	my_number = "13579"
+    	winning_numbers = ["13570","77777","13509"]
+    	assert_equal(["13570","13509"], three_out_four(my_number, winning_numbers))
+    end
+
+
+    def test_X_of_Y_remix_1
+    	my_number = "135791"
+    	winning_numbers = ["135792","777677","135701"]
+    	assert_equal(["135792","135701"], three_out_four(my_number, winning_numbers))
+    end
+end
 
