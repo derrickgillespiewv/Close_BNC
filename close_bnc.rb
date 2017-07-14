44f7c0743e9c24d58489cf295af666babcb9dd95
Derrick Gillespie <derrickgillespiewv@gmail.com>
2017-07-14 10:39:56 -0400

    Three out of Four

--- close_bnc.rb
+++ close_bnc.rb
@@ -3,15 +3,76 @@
 # b = ["1234","5678","9012","1357"].sample
 # end 
 
+# def lotto_func(a, b)
+# same = [] 
+# b.each_with_index do |value, index|
+# 	if a == value
+# 		same << a
+# 		p "You Win!"
+# 	else
+# 		p "You lose, buy more tickets!"
+# 	end
+# end
+# same
+# end
+
 def lotto_func(a, b)
 same = [] 
 b.each_with_index do |value, index|
 	if a == value
 		same << a
-		p "You Win!"
+		# p "You Win!"
 	else
-		p "You lose, buy more tickets!"
+		# p "You lose, buy more tickets!"
 	end
 end
 same
 end
+
+
+def one_off(a, b)
+	index_position = 0
+	count = 0
+	
+	b.each do |winning_ticket|
+	count = 0
+	index_position = 0
+	4.times do 
+		if a[index_position] == winning_ticket[index_position]
+		count += 1
+		end
+		index_position += 1
+	end
+end
+	count == 3
+end
+
+
+# def off_by_one(a, b)
+#   count = 0
+#   index_a = 0
+#    b.length.times do
+#    index_b = 0
+#     b.length.times do
+#       if a[index_a] == b[index_b]
+#         count += 1
+#       end
+#       index_b+=1
+#   end
+#   index_a+=1
+# end
+#   count == b.length - 1
+# end
+
+
+# def three_out_four(a, b)
+#   same = []
+#   b.each do |y|
+#     if off_by_one(a, y) == true
+#       same << y
+#     end
+#   end
+#   winning
+# end
+
+# one_off "1234", ["1234","5678", "9012", "5555"]
--- close_bnc_test.rb
+++ close_bnc_test.rb
@@ -11,4 +11,30 @@ class TestClose_BNC < Minitest::Test
 		winning_numbers = ["1234","5678","9012","1357"]
 		assert_equal(["1357"], lotto_func(my_number, winning_numbers))
 	end 
-end 	
+
+	def test_no_winning_tickets
+	    my_number = "1357"
+        winning_numbers = ["1234","5678","9012"]
+        assert_equal(false , one_off(my_number, winning_numbers))
+    end
+       
+    def test_close_match
+    	my_number = "1357"
+    	winning_numbers = ["1358"]
+    	assert_equal(true, one_off(my_number, winning_numbers))
+	end  
+
+	def test_not_quite_close
+		my_number = "1357"
+    	winning_numbers = ["1449"]
+    	assert_equal(false, one_off(my_number, winning_numbers))
+	end  
+
+    def test_winning_one_off
+    	my_number = "1357"
+    	winning_numbers = ["1358","3157","1350"]
+    	assert_equal(true, one_off(my_number, winning_numbers))
+	end
+end
+
+
